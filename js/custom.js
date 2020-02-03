be05cc24e6588fa325e9a196e5fb2d2771ec86e5
mohamed elsayed <mohamedelsayedhussein0@gmail.com>
2020-02-03 15:45:25 +0200

    dvd

--- js/custom.js
+++ js/custom.js
@@ -8,6 +8,24 @@ $(document).ready(function () {
         } else {
             $('.scroll-to-top').removeClass('show');
         }
+        $('.chart').each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $('.chart').easyPieChart({
+                    lineWidth: 6,
+                    lineCap: 'square',
+                    barColor: "#22a7f0",
+                    scaleColor: '#22a7f0',
+                    trackColor: '#555',
+                    easing: 'easeOutBounce',
+                    animate: 2500,
+                    onStep: function (from, to, percent) {
+                        $(this.el).find('.percent').text(Math.round(percent));
+                    }
+                });
+            }
+        });
     });
 
     $('.scroll-to-top').click(function (e) {
